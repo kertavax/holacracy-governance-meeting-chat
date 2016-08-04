8a4514b860fe435fa1e8efe31942259db3dc6d2b
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-04 18:11:27 -0400

    Update validation tests according to updated PDF on holacracyone.org

--- js/main.js
+++ js/main.js
@@ -30,19 +30,20 @@ $(document).ready(function() {
 		var progressStatus = "";
 
 		while(isValidObjectionTest_1) {
-			var progressStatus = "1/6\n";
-			var testObjectionText = "Is that a reason this causes harm or moves us backward? {{ Type \"1\" }}\n…OR…\nIs it a better idea or something else we should consider as well? {{ Type \"2\" }}";
+			var progressStatus = "1/5\n";
+			var testObjectionText = "Is your concern a reason the proposal causes harm… {{ Type \"1\" }}\n…OR…\nIs your concern the proposal is unneeded or incomplete? {{ Type \"2\" }}";
 			var response = "";
+			var objectionInvalidReason = "The reason being …";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
 			switch(questionPrompt) {
 				case "1":
-					response = "You said that the proposal in its current form would cause harm or move us backward. Moving to next objection validation test.";
+					response = "You said that the proposal in its current form would cause harm. Moving to next objection validation test.";
 					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
 					isValidObjectionTest_1 = false;
 					break;
 				case "2":
-					response = "You said that your objection is a better idea or something else we should consider as well. Objection is invalid."
-					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
+					response = "You said that your concern is that the proposal is unneeded or incomplete. Objection is invalid.";
+					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response + "<br />" + objectionInvalidReason;
 					// refactor below repetitive false-ifiers
 					isValidObjectionTest_1 = false;
 					isValidObjectionTest_2 = false;
@@ -66,19 +67,19 @@ $(document).ready(function() {
 		}
 
 		while(isValidObjectionTest_2) {
-			var progressStatus = "2/6\n";
-			var testObjectionText = "Would that issue be created by adopting this proposal? {{ Type \"1\" }}\n…OR…\nIs it already an issue, even if we don’t adopt this proposal? {{ Type \"2\" }}";
+			var progressStatus = "2/5\n";
+			var testObjectionText = "Is your concern created by this proposal… {{ Type \"1\" }}\n…OR…\nIs it already a concern or tension, even if the proposal were dropped? {{ Type \"2\" }}";
 			var response = "";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
 			switch(questionPrompt) {
 				case "1":
-					response = "You said that the proposal in its current form would create a new tension. Moving to next objection validation test."
+					response = "You said that your concern is created by the proposal in its current form. Moving to next objection validation test."
 					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
 					isValidObjectionTest_2 = false;
 					break;
 				case "2":
-					response = "You said that your objection is related to something that is already a tension and that adopting this proposal would not affect that particular tension. Objection is invalid."
-					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
+					response = "You said that your concern is an already tension, even if the current proposal were dropped. Objection is invalid."
+					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response + "<br />" + objectionInvalidReason;
 					// refactor below repetitive false-ifiers
 					isValidObjectionTest_2 = false;
 					isValidObjectionTest_3 = false;
@@ -100,19 +101,19 @@ $(document).ready(function() {
 		}
 
 		while(isValidObjectionTest_3) {
-			var progressStatus = "3/6\n";
-			var testObjectionText = "Is that based on presently-known data? {{ Type \"1\" }}\n…OR…\nAre you anticipating that it might happen? {{ Type \"2\" }}";
+			var progressStatus = "3/5\n";
+			var testObjectionText = "Do you know, based on present data, that this impact will occur… {{ Type \"1\" }}\n…OR…\nAre you anticipating this impact is likely to occur? {{ Type \"2\" }}";
 			var response = "";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
 			switch(questionPrompt) {
 				case "1":
-					response = "You said that your objection is based on presently-known data. Skipping 3b – Moving to next objection validation test."
+					response = "You said that your concern is based on presently-known data. Skipping 3b – Moving to next objection validation test."
 					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
 					isValidObjectionTest_3 = false;
 					isValidObjectionTest_3b = false;
 					break;
 				case "2":
-					response = "You said that you are anticipating that something might happen. Moving to 3b objection validation test."
+					response = "You said that you are anticipating this impact is likely to occur. Moving to 3b objection validation test."
 					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
 					isValidObjectionTest_3 = false;
 					break;
@@ -129,19 +130,21 @@ $(document).ready(function() {
 		}
 
 		while(isValidObjectionTest_3b) {
-			var progressStatus = "4/6\n";
-			var testObjectionText = "If anticipated: is there a reason we can’t adapt once we get more data? {{ Type \"1\" }}\n…OR…\nIs it safe enough to try, knowing we can revisit it anytime? {{ Type \"2\" }}";
+			var progressStatus = "3/5\n";
+			var testObjectionText = "If anticipated: Is it safe enough to try, knowing we can revisit it anytime… {{ Type \"1\" }}\n…OR…\nIs it not safe enough to try, because significant harm could happen before we can adapt? {{ Type \"2\" }}";
 			var response = "";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
 			switch(questionPrompt) {
 				case "1":
-					response = "You said that there is a reason we can’t adapt proposal in current form once we get more data. Moving to next objection validation test."
-					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
+					response = "You said that the proposal in its current form is safe enough to try, knowing we can revisit it anytime. Objection is invalid."
+					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response + "<br />" + objectionInvalidReason;
 					isValidObjectionTest_3b = false;
+					isValidObjectionTest_4 = false;
+					isValidObjectionTest_5 = false;
 					break;
 				case "2":
-					response = "You said that the proposal in its current form is safe enough to try, knowing we can revisit it anytime. Objection is invalid."
-					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
+					response = "You said that the proposal in its current form is not safe enough to try, because significant harm could happen before we can adapt. Objection is invalid."
+					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response + "<br />" + objectionInvalidReason;
 					// refactor below repetitive false-ifiers
 					isValidObjectionTest_3b = false;
 					isValidObjectionTest_4 = false;
@@ -159,8 +162,8 @@ $(document).ready(function() {
 		}
 
 		while(isValidObjectionTest_4) {
-			var progressStatus = "5/6\n";
-			var testObjectionText = "Does it limit one of your Roles? Which one? {{ Type \"1\" }}\n…OR…\nAre you trying to help another Role or the Circle?? {{ Type \"2\" }}";
+			var progressStatus = "4/5\n";
+			var testObjectionText = "Would the proposal limit one of your roles… {{ Type \"1\" }}\n…OR…\nAre you trying to help another role or the circle in general? {{ Type \"2\" }}";
 			var response = "";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
 			switch(questionPrompt) {
@@ -170,8 +173,8 @@ $(document).ready(function() {
 					isValidObjectionTest_4 = false;
 					break;
 				case "2":
-					response = "You said that you are trying to help another Role or the Circle in general. Objection is invalid."
-					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response;
+					response = "You said that you are trying to help another Role or the circle in general. Objection is invalid."
+					document.getElementById('demo').innerHTML = "Just completed " + progressStatus + "<br />" + response + "<br />" + objectionInvalidReason;
 					// refactor below repetitive false-ifiers
 					isValidObjectionTest_4 = false;
 					isValidObjectionTest_5 = false;
@@ -187,7 +190,7 @@ $(document).ready(function() {
 		}
 
 		while(isValidObjectionTest_5) {
-			var progressStatus = "6/6\n";
+			var progressStatus = "5/5\n";
 			var testObjectionText = "Is the proposal adhering to Holacracy Constitution rules of creating, amending or removing Roles’ accountabilities and domains? {{ Type \"Y\" for Yes, \"N\" for No }}.";
 			var response = "";
 			var questionPrompt = prompt(progressStatus + testObjectionText);
